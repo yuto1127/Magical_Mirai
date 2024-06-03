54c40570b8e8386068e0e57f53d4596ae71ed0d3
HN-YM <166769807+HN-YM@users.noreply.github.com>
2024-06-03 18:53:33 +0900

    Changed any codes.

--- MagiMira2024/src/js/main.js
+++ MagiMira2024/src/js/main.js
@@ -1,3 +1,5 @@
+var time = 0; //追加箇所 HN
+
 document.addEventListener("DOMContentLoaded", () => {
     /* TextAlive Playerのインスタンスを作成 */
     const player = new TextAliveApp.Player({
@@ -133,10 +135,85 @@ document.addEventListener("DOMContentLoaded", () => {
     );
 
     /* 再生ボタン */
+    bg_image = document.getElementById("bg_image"); //追加箇所 HN
+    scene = 0; //追加箇所 HN
+    bg_tmp = "../../img/bg_img/仮背景.jpg"; //追加箇所 HN
     const playButton = document.getElementById("play-button");
     playButton.addEventListener("click", () => {
         player.requestPlay(); // 再生を要求する
         playButton.style.display = "none"; // 再生ボタンを非表示にする
+
+        //以下追加処理 HN
+        // 0.1秒ごとに実行
+        setInterval(() => {
+            time+=1;
+            if(scene == 0 && (0 < time && time <= 110)){
+                scene = 1;
+                bg_image.src = "../../img/bg_img/背景_1_9.jpg";
+            }else if(scene == 1 && (110 < time && time <= 200)){
+                scene = 2;
+                bg_image.src = bg_tmp; //仮背景に設定
+            }else if(scene == 2 && (200 < time && time <= 310)){
+                scene = 3;
+                bg_image.src = bg_tmp; //仮背景に設定
+            }else if(scene == 3 && (310 < time && time <= 330)){
+                scene = 4;
+                bg_image.src = bg_tmp; //仮背景に設定
+            }else if(scene == 4 && (330 < time && time <= 380)){
+                scene = 5;
+                bg_image.src = bg_tmp; //仮背景に設定
+            }else if(scene == 5 && (380 < time && time <= 410)){
+                scene = 6;
+                bg_image.src = bg_tmp; //仮背景に設定
+            }else if(scene == 6 && (410 < time && time <= 490)){
+                scene = 7;
+                bg_image.src = bg_tmp; //仮背景に設定
+            }else if(scene == 7 && (490 < time && time <= 510)){
+                scene = 8;
+                bg_image.src = bg_tmp; //仮背景に設定
+            }else if(scene == 8 && (510 < time && time <= 610)){
+                scene = 9;
+                bg_image.src = "../../img/bg_img/背景_1_9.jpg";
+            }else if(scene == 9 && (610 < time && time <= 720)){
+                scene = 10;
+                bg_image.src = bg_tmp; //仮背景に設定
+            }else if(scene == 10 && (720 < time && time <= 810)){
+                scene = 11;
+                bg_image.src = bg_tmp; //仮背景に設定
+            }else if(scene == 11 && (810 < time && time <= 930)){
+                scene = 12;
+                bg_image.src = bg_tmp; //仮背景に設定
+            }else if(scene == 12 && (930 < time && time <= 1020)){
+                scene = 13;
+                bg_image.src = bg_tmp; //仮背景に設定
+            }else if(scene == 13 && (1020 < time && time <= 1060)){
+                scene = 14;
+                bg_image.src = bg_tmp; //仮背景に設定
+            }else if(scene == 14 && (1060 < time && time <= 1120)){
+                scene = 15;
+                bg_image.src = bg_tmp; //仮背景に設定
+            }else if(scene == 15 && (1120 < time && time <= 1210)){
+                scene = 16;
+                bg_image.src = bg_tmp; //仮背景に設定
+            }else if(scene == 16 && (1210 < time && time <= 1220)){
+                scene = 17;
+                bg_image.src = bg_tmp; //仮背景に設定
+            }else if(scene == 17 && (1220 < time && time <= 1310)){
+                scene = 18;
+                bg_image.src = bg_tmp; //仮背景に設定
+            }else if(scene == 18 && (1310 < time && time <= 1330)){
+                scene = 19;
+                bg_image.src = bg_tmp; //仮背景に設定
+            }else if(scene == 19 && (1330 < time && time <= 1410)){
+                scene = 20;
+                bg_image.src = bg_tmp; //仮背景に設定
+            }else if(scene == 20 && (1410 < time && time <= 1530)){
+                scene = 21;
+                bg_image.src = bg_tmp; //仮背景に設定
+            }else{
+                console.log("End");
+            }
+        },100)//ここまで HN
     });
 
     /* ロード完了後にロード画面を非表示にする */
