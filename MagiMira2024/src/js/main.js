d748bfad02596c33e2bd1065ef8f14e9df0ec094
HN-YM <166769807+HN-YM@users.noreply.github.com>
2024-06-04 12:05:49 +0900

    test

--- MagiMira2024/src/js/main.js
+++ MagiMira2024/src/js/main.js
@@ -20,116 +20,124 @@ document.addEventListener("DOMContentLoaded", () => {
         // 歌詞と発話時間の組み合わせをここに追加
          // １番Aメロ＃１
 
-         {text:"轟く1・2・3",time:0.8},
+         {text:"轟く1・2・3",time:8},
 
-         {text:"ほらよーいどんで飛び込んで",time:3.9},
+         {text:"ほらよーいどんで飛び込んで",time:39},
  
-         {text:"はらりと彩った",time:6.5},
+         {text:"はらりと彩った",time:65},
  
-         {text:"未来　未来　未来",time:8.8},
+         {text:"未来　未来　未来",time:88},
  
-         {text:"Oh Oh",time:11},
+         {text:"Oh Oh",time:110},
  
          // １番Aメロ＃２
  
-         {text:"いちにっさんしで色めいて",time:11.5},
+         {text:"いちにっさんしで色めいて",time:115},
  
-         {text:"この音が巡る",time:14},
+         {text:"この音が巡る",time:140},
  
-         {text:"巡るとき",time:15.5},
+         {text:"巡るとき",time:155},
  
-         {text:"心の全部がときめいて",time:17},
+         {text:"心の全部がときめいて",time:170},
  
-         {text:"期待したい　したい",time:19},
+         {text:"期待したい　したい",time:190},
  
          // １番Bメロ
  
-         {text:"想定外の不安要素も",time:21.3},
+         {text:"想定外の不安要素も",time:213},
  
-         {text:"展開は低解像度",time:24},
+         {text:"展開は低解像度",time:240},
  
-         {text:"伝えたい言の葉が響くまで",time:27},
+         {text:"伝えたい言の葉が響くまで",time:270},
  
          // サビ
  
-         {text:"初の音が奏でる世界",time:31.5},
+         {text:"初の音が奏でる世界",time:315},
  
-         {text:"彩るこの視界",time:36},
+         {text:"彩るこの視界",time:360},
  
-         {text:"セカイ　セカイ　セカイ",time:39.3},
+         {text:"セカイ　セカイ　セカイ",time:393},
  
-         {text:"間違いなんかない",time:41.5},
+         {text:"間違いなんかない",time:415},
  
-         {text:"全ては君次第",time:43.9},
+         {text:"全ては君次第",time:439},
  
-         {text:"今日は過去の未来",time:46.6},
+         {text:"今日は過去の未来",time:466},
  
-         {text:"ミライ　ミライ　ミライ",time:49.9},
+         {text:"ミライ　ミライ　ミライ",time:499},
  
          // ーーーーーー間奏ーーーーーーー
  
          // ２番Aメロ
  
-         {text:"音と音の鼓動が響き合って",time:62},
+         {text:"音と音の鼓動が響き合って",time:620},
  
-         {text:"連なって繋いで生まれ落ちて",time:65},
+         {text:"連なって繋いで生まれ落ちて",time:650},
  
-         {text:"記すノートも",time:67.5},
+         {text:"記すノートも",time:675},
  
-         {text:"痛むハートの",time:68.6},
+         {text:"痛むハートの",time:686},
  
-         {text:"照らすライトになっていく",time:70},
+         {text:"照らすライトになっていく",time:700},
  
          // ２番Bメロ
  
-         {text:"ABCからXYZ",time:73},
+         {text:"ABCからXYZ",time:730},
  
-         {text:"レイアウトは捨て去って",time:75.5},
+         {text:"レイアウトは捨て去って",time:755},
  
-         {text:"鳴らしたい音",time:77.8},
+         {text:"鳴らしたい音",time:778},
  
-         {text:"伝えたいこと",time:79},
+         {text:"伝えたいこと",time:790},
  
-         {text:"創造も超えて宇宙の向こう側",time:80.2},
+         {text:"創造も超えて宇宙の向こう側",time:802},
  
          // ーーーーーーー間奏ーーーーーーー
  
          // サビ前オチ
  
-         {text:"初の音が奏でる世界",time:93},
+         {text:"初の音が奏でる世界",time:930},
  
-         {text:"彩るこの視界",time:97.8},
+         {text:"彩るこの視界",time:978},
  
-         {text:"セカイ　セカイ　セカイ",time:100.3},
+         {text:"セカイ　セカイ　セカイ",time:1003},
  
-         {text:"君次第",time:103},
+         {text:"君次第",time:1030},
  
-         {text:"全ては君の作り上げた",time:105},
+         {text:"全ては君の作り上げた",time:1050},
  
-         {text:"長い長い世界の続きを見よう",time:108.5},
+         {text:"長い長い世界の続きを見よう",time:1085},
  
          // ラスサビ
  
-         {text:"High!!",time:113.5},
+         {text:"High!!",time:1135},
  
-         {text:"夢見心地のDIVE!!",time:115.8},
+         {text:"夢見心地のDIVE!!",time:1158},
  
-         {text:"思うがままのLIFE!!",time:117.8},
+         {text:"思うがままのLIFE!!",time:1178},
  
-         {text:"セカイ　セカイ　セカイ",time:121},
+         {text:"セカイ　セカイ　セカイ",time:1210},
  
-         {text:"間違いなんかない",time:123},
+         {text:"間違いなんかない",time:1230},
  
-         {text:"全ては君次第",time:125.5},
+         {text:"全ては君次第",time:1255},
  
-         {text:"導くこの未来",time:128},
+         {text:"導くこの未来",time:1280},
  
-         {text:"ミライ　ミライ　ミライ",time:131.5},
+         {text:"ミライ　ミライ　ミライ",time:1315},
  
-         {text:"初の音が重なる世界",time:133.4},
+         {text:"初の音が重なる世界",time:1334},
  
-         {text:"終わりのない時代",time:139},
+         {text:"終わりのない時代",time:1390},
     ];
+
+    var animTextList = [];
+
+    for(var i=0;i<phrases.length;i++){
+        animTextList.push(new AnimationTextBase(phrases[i].text,"20px","#000000","200px","100px",0,1000,0));
+        animTextList.setID("text_"+i);
+        animTextList[i].generate();
+    }
 
     /* GSAPアニメーションのセットアップ */
     const lyricsAnimation = gsap.fromTo("#lyrics", 
@@ -138,8 +146,9 @@ document.addEventListener("DOMContentLoaded", () => {
     );
 
     /* 再生ボタン */
-    const bg_image = document.getElementById("bg_image"); //追加箇所 HN
     var scene = 0; //追加箇所 HN
+    var animTextIndex = 0; //追加箇所 HN
+    const bg_image = document.getElementById("bg_image"); //追加箇所 HN
     const bg_tmp = "../../img/bg_img/仮背景.jpg"; //追加箇所 HN
     const playButton = document.getElementById("play-button");
     playButton.addEventListener("click", () => {
@@ -216,6 +225,11 @@ document.addEventListener("DOMContentLoaded", () => {
             }else if(scene == 21){
                 scene = 22;
                 console.log("End");
+            }
+
+            if(time == phrases[animTextIndex].time){
+                animTextList[animTextIndex].play();
+                animTextIndex++;
             }
         },100)//ここまで HN
     });
