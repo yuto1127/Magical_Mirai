7a1ad7653e370aa1711e0a231ea0c67bfac84ff4
yu_to <61904855+yuto1127@users.noreply.github.com>
2024-06-06 00:06:59 +0900

    p5.js

--- EffectTest/p5.js
+++ EffectTest/p5.js
@@ -34,12 +34,12 @@ function createParticle(x0, y0) {
   const [x, y, color] = [
     x0 + Math.random(-size / 4, size / 4),
     y0 + Math.random(-size / 4, size / 4),
-    [random(90, 290), 50, 50, 100],
+    [Math.random(90, 290), 50, 50, 100],
   ];
   const lifetime = Math.random(1, 3) * 60;
   const [dx, dy, ds, da] = [
-    random(-100, 100) / lifetime,
-    random(-100, 100) / lifetime,
+    Math.random(-100, 100) / lifetime,
+    Math.random(-100, 100) / lifetime,
     size / lifetime,
     100 / lifetime,
   ];
