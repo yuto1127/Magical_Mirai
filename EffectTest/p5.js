b22dfea2dc9bf17515c6e580e0219366286ddc4d
yu_to <61904855+yuto1127@users.noreply.github.com>
2024-06-06 00:09:20 +0900

    p5.js

--- EffectTest/p5.js
+++ EffectTest/p5.js
@@ -1,8 +1,67 @@
+// let particleList = [];
+
+// function setup() {
+//   createCanvas(windowWidth, windowHeight);
+
+//   noStroke();
+//   colorMode(HSB, 360, 100, 100, 100);
+// }
+
+// function draw() {
+//   background(80);
+
+//   for (const particle of particleList) {
+//     fill(particle.color);
+//     circle(particle.x, particle.y, particle.size);
+//     particle.x -= particle.dx;
+//     particle.y -= particle.dy;
+//     particle.size -= particle.ds;
+//     particle.color[3] -= particle.da;
+//     particle.lifetime -= 1;
+//   }
+
+//   particleList = particleList.filter((particle) => {
+//     return particle.lifetime > 0;
+//   });
+// }
+
+// function mouseMoved() {
+//   createParticle(mouseX, mouseY);
+// }
+
+// function createParticle(x0, y0) {
+//   const size = Math.random(10, 60);
+//   const [x, y, color] = [
+//     x0 + Math.random(-size / 4, size / 4),
+//     y0 + Math.random(-size / 4, size / 4),
+//     [Math.random(90, 290), 50, 50, 100],
+//   ];
+//   const lifetime = Math.random(1, 3) * 60;
+//   const [dx, dy, ds, da] = [
+//     Math.random(-100, 100) / lifetime,
+//     Math.random(-100, 100) / lifetime,
+//     size / lifetime,
+//     100 / lifetime,
+//   ];
+
+//   const particle = { x, y, size, dx, dy, ds, color, da, lifetime };
+//   particleList.push(particle);
+// }
+// window.addEventListener('mousemove', function (e) {
+//     var x = e.clientX;
+//     var y = e.clientY;
+//     createParticle(x, y);
+//   });
+//   document.body.addEventListener('touchmove', function (e) {
+//     var x = e.touches[0].clientX;
+//     var y = e.touches[0].clientY;
+//     e.preventDefault();
+//     createParticle(x, y);
+//   });
 let particleList = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-
   noStroke();
   colorMode(HSB, 360, 100, 100, 100);
 }
@@ -30,16 +89,16 @@ function mouseMoved() {
 }
 
 function createParticle(x0, y0) {
-  const size = Math.random(10, 60);
+  const size = Math.random() * 50 + 10;
   const [x, y, color] = [
-    x0 + Math.random(-size / 4, size / 4),
-    y0 + Math.random(-size / 4, size / 4),
-    [Math.random(90, 290), 50, 50, 100],
+    x0 + Math.random() * size - size / 2,
+    y0 + Math.random() * size - size / 2,
+    [Math.random() * 290 + 90, 50, 50, 100],
   ];
-  const lifetime = Math.random(1, 3) * 60;
+  const lifetime = Math.random() * 180 + 60;
   const [dx, dy, ds, da] = [
-    Math.random(-100, 100) / lifetime,
-    Math.random(-100, 100) / lifetime,
+    (Math.random() - 0.5) * 100 / lifetime,
+    (Math.random() - 0.5) * 100 / lifetime,
     size / lifetime,
     100 / lifetime,
   ];
@@ -47,14 +106,16 @@ function createParticle(x0, y0) {
   const particle = { x, y, size, dx, dy, ds, color, da, lifetime };
   particleList.push(particle);
 }
+
 window.addEventListener('mousemove', function (e) {
-    var x = e.clientX;
-    var y = e.clientY;
-    createParticle(x, y);
-  });
-  document.body.addEventListener('touchmove', function (e) {
-    var x = e.touches[0].clientX;
-    var y = e.touches[0].clientY;
-    e.preventDefault();
-    createParticle(x, y);
-  });
+  var x = e.clientX;
+  var y = e.clientY;
+  createParticle(x, y);
+});
+
+document.body.addEventListener('touchmove', function (e) {
+  var x = e.touches[0].clientX;
+  var y = e.touches[0].clientY;
+  e.preventDefault();
+  createParticle(x, y);
+});
