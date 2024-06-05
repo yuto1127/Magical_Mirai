c06cdab0ddb302247ffae99ee1486ac546e3416f
Sho0428 <f23aa006@chuo.ac.jp>
2024-06-06 00:31:37 +0900

    p5.js

--- EffectTest/p5.js
+++ EffectTest/p5.js
@@ -1,60 +1,41 @@
-let particleList = [];
-
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-  noStroke();
-  colorMode(HSB, 360, 100, 100, 100);
-}
-
-function draw() {
-  background(80);
-
-  for (const particle of particleList) {
-    fill(particle.color);
-    circle(particle.x, particle.y, particle.size);
-    particle.x -= particle.dx;
-    particle.y -= particle.dy;
-    particle.size -= particle.ds;
-    particle.color.setAlpha(particle.color._array[3] - particle.da / 100);
-    particle.lifetime -= 1;
-  }
-
-  particleList = particleList.filter((particle) => {
-    return particle.lifetime > 0;
+function createParticle (x, y) {
+  var size = Math.random() * 50 + 10;
+  
+  x -= (size / 2);
+  y -= (size / 2);
+  
+  var particle = document.createElement('div');
+  document.body.appendChild(particle);
+  
+  TweenMax.set(particle, {
+    x: x, 
+    y: y,
+    width: size,
+    height: size,
+    background: function () {
+      return `hsl(${Math.random() *90+200}, 50%, 50%)`
+    }
   });
-}
-
-function mouseMoved() {
-  createParticle(mouseX, mouseY);
-}
-
-function createParticle(x0, y0) {
-  const size = Math.random() * 50 + 10;
-  const x = x0 + Math.random() * size - size / 2;
-  const y = y0 + Math.random() * size - size / 2;
-  const particleColor = color(Math.random() * 290 + 90, 50, 50, 100);  // color関数を適切に使用
-  const lifetime = Math.random() * 180 + 60;
-  const dx = (Math.random() - 0.5) * 100 / lifetime;
-  const dy = (Math.random() - 0.5) * 100 / lifetime;
-  const ds = size / lifetime;
-  const da = 100 / lifetime;
-
-  const particle = { x, y, size, dx, dy, ds, color: particleColor, da, lifetime };
-  particleList.push(particle);
+  TweenMax.to(particle, Math.random() * 2 + 1, {
+    x: x + (Math.random() - 0.5) * 200,
+    y: y + (Math.random() - 0.5) * 200,
+    opacity: 0,
+    scale:0,
+    ease: Power2.easeOut,
+    onComplete: function () {
+      document.body.removeChild(particle);
+    }
+  })
 }
 
 window.addEventListener('mousemove', function (e) {
   var x = e.clientX;
   var y = e.clientY;
   createParticle(x, y);
-  draw();
 });
-
 document.body.addEventListener('touchmove', function (e) {
   var x = e.touches[0].clientX;
   var y = e.touches[0].clientY;
   e.preventDefault();
   createParticle(x, y);
-  draw();
 });
-setup();
