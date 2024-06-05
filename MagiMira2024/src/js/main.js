6a8adbc0e488449539b00e9462c1e328dc5b15ff
HN-YM <166769807+HN-YM@users.noreply.github.com>
2024-06-05 22:42:21 +0900

    test

--- MagiMira2024/src/js/main.js
+++ MagiMira2024/src/js/main.js
@@ -13,8 +13,7 @@ document.addEventListener("DOMContentLoaded", () => {
         mediaElement: document.querySelector("#media") // 新しいメディア要素を指定
         
     });
-    
-
+    player.volume = 50;
     // 未使用↓
     /* 歌詞表示エリア */
     // const lyricsElement = document.getElementById("lyrics");
