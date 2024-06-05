b979c8827ce37cf3fc6010cb1e7e20bbeda84912
yu_to <61904855+yuto1127@users.noreply.github.com>
2024-06-06 00:27:41 +0900

    test

--- EffectTest/p5.js
+++ EffectTest/p5.js
@@ -10,17 +10,17 @@ function draw() {
   background(80);
 
   for (const particle of particleList) {
-    fill(particle.color.levels[0], particle.color.levels[1], particle.color.levels[2], particle.color.levels[3]);
+    fill(particle.color);
     circle(particle.x, particle.y, particle.size);
     particle.x -= particle.dx;
     particle.y -= particle.dy;
     particle.size -= particle.ds;
-    particle.color.levels[3] -= particle.da;
+    particle.color.setAlpha(particle.color._array[3] - particle.da / 100);
     particle.lifetime -= 1;
   }
 
   particleList = particleList.filter((particle) => {
-    return particle.lifetime > 0 && particle.color.levels[3] > 0;
+    return particle.lifetime > 0;
   });
 }
 
@@ -32,14 +32,14 @@ function createParticle(x0, y0) {
   const size = Math.random() * 50 + 10;
   const x = x0 + Math.random() * size - size / 2;
   const y = y0 + Math.random() * size - size / 2;
-  const particleColor = color(Math.random() * 290 + 90, 50, 50, 100);  
+  const particleColor = color(Math.random() * 290 + 90, 50, 50, 100);  // color関数を適切に使用
   const lifetime = Math.random() * 180 + 60;
   const dx = (Math.random() - 0.5) * 100 / lifetime;
   const dy = (Math.random() - 0.5) * 100 / lifetime;
   const ds = size / lifetime;
   const da = 100 / lifetime;
 
-  const particle = { x, y, size, dx, dy, ds, color, da, lifetime };
+  const particle = { x, y, size, dx, dy, ds, color: particleColor, da, lifetime };
   particleList.push(particle);
 }
 
@@ -57,5 +57,4 @@ document.body.addEventListener('touchmove', function (e) {
   createParticle(x, y);
   draw();
 });
-
 setup();
