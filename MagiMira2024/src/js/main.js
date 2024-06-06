a84fc9f0dcb32290a64cb1e81c78185eb01433b9
HN-YM <166769807+HN-YM@users.noreply.github.com>
2024-06-07 01:17:57 +0900

    test

--- MagiMira2024/src/js/main.js
+++ MagiMira2024/src/js/main.js
@@ -3,6 +3,209 @@ import {AnimationTextBase} from './animations/AnimationTextBase.js';
 import {FadeInClass} from './animations/FadeInClass.js';
 
 var time = 0; //追加箇所 HN
+
+// 歌詞の配列データ
+const phrases = [
+    // 歌詞と発話時間の組み合わせをここに追加
+     // １番Aメロ＃１
+
+     {text:"轟く1・2・3",time:8},
+
+     {text:"ほらよーいどんで飛び込んで",time:39},
+
+     {text:"はらりと彩った",time:65},
+
+     {text:"未来　未来　未来",time:88},
+
+     {text:"Oh Oh",time:110},
+
+     // １番Aメロ＃２
+
+     {text:"いちにっさんしで色めいて",time:115},
+
+     {text:"この音が巡る",time:140},
+
+     {text:"巡るとき",time:155},
+
+     {text:"心の全部がときめいて",time:170},
+
+     {text:"期待したい　したい",time:190},
+
+     // １番Bメロ
+
+     {text:"想定外の不安要素も",time:213},
+
+     {text:"展開は低解像度",time:240},
+
+     {text:"伝えたい言の葉が響くまで",time:270},
+
+     // サビ
+
+     {text:"初の音が奏でる世界",time:315},
+
+     {text:"彩るこの視界",time:360},
+
+     {text:"セカイ　セカイ　セカイ",time:393},
+
+     {text:"間違いなんかない",time:415},
+
+     {text:"全ては君次第",time:439},
+
+     {text:"今日は過去の未来",time:466},
+
+     {text:"ミライ　ミライ　ミライ",time:499},
+
+     // ーーーーーー間奏ーーーーーーー
+
+     // ２番Aメロ
+
+     {text:"音と音の鼓動が響き合って",time:620},
+
+     {text:"連なって繋いで生まれ落ちて",time:650},
+
+     {text:"記すノートも",time:675},
+
+     {text:"痛むハートの",time:686},
+
+     {text:"照らすライトになっていく",time:700},
+
+     // ２番Bメロ
+
+     {text:"ABCからXYZ",time:730},
+
+     {text:"レイアウトは捨て去って",time:755},
+
+     {text:"鳴らしたい音",time:778},
+
+     {text:"伝えたいこと",time:790},
+
+     {text:"創造も超えて宇宙の向こう側",time:802},
+
+     // ーーーーーーー間奏ーーーーーーー
+
+     // サビ前オチ
+
+     {text:"初の音が奏でる世界",time:930},
+
+     {text:"彩るこの視界",time:978},
+
+     {text:"セカイ　セカイ　セカイ",time:1003},
+
+     {text:"君次第",time:1030},
+
+     {text:"全ては君の作り上げた",time:1050},
+
+     {text:"長い長い世界の続きを見よう",time:1085},
+
+     // ラスサビ
+
+     {text:"High!!",time:1135},
+
+     {text:"夢見心地のDIVE!!",time:1158},
+
+     {text:"思うがままのLIFE!!",time:1178},
+
+     {text:"セカイ　セカイ　セカイ",time:1210},
+
+     {text:"間違いなんかない",time:1230},
+
+     {text:"全ては君次第",time:1255},
+
+     {text:"導くこの未来",time:1280},
+
+     {text:"ミライ　ミライ　ミライ",time:1315},
+
+     {text:"初の音が重なる世界",time:1334},
+
+     {text:"終わりのない時代",time:1390},
+];
+
+var animTextList = [];
+
+var scene_info_index = 1;
+const scene_info = [
+    {scene_num:0,scene_time:-1,bg_image_pass:"none"},
+    {scene_num:1,scene_time:0,bg_image_pass:"bg_1_13.GIF"},
+    {scene_num:2_01,scene_time:115,bg_image_pass:"bg_2_導入_アニメ.gif"},
+    {scene_num:2_02,scene_time:115+16,bg_image_pass:"bg_2_導入_静止画.jpg"},
+    {scene_num:2_03,scene_time:166,bg_image_pass:"bg_2_終わり_アニメ.gif"},
+    {scene_num:2_04,scene_time:166+11,bg_image_pass:"bg_2_終わり_静止画.jpg"},
+    {scene_num:3_01,scene_time:217,bg_image_pass:"bg_3_10_導入_ディザリング.gif"},
+    {scene_num:3_02,scene_time:217+30,bg_image_pass:"bg_3_10_静止画_導入.jpg"},
+    {scene_num:3_03,scene_time:242,bg_image_pass:"bg_3_10_終了_ディザリング.gif"},
+    {scene_num:3_04,scene_time:242+20,bg_image_pass:"bg_3_10_静止画_終了.jpg"},
+    {scene_num:4,scene_time:313,bg_image_pass:"bg_4_6_8_17_19.jpg"},
+    {scene_num:5,scene_time:345,bg_image_pass:"bg_5_7_12_16_18_静止画_1.jpg"},
+    {scene_num:6,scene_time:392,bg_image_pass:"bg_4_6_8_17_19.jpg"},
+    {scene_num:7,scene_time:412,bg_image_pass:"bg_5_7_12_16_18_静止画_2.jpg"},
+    {scene_num:8,scene_time:482,bg_image_pass:"bg_4_6_8_17_19.jpg"},
+    {scene_num:9_01,scene_time:530,bg_image_pass:"bg_9$1.gif"},
+    {scene_num:9_02,scene_time:531,bg_image_pass:"bg_9$2.jpg"},
+    {scene_num:9_03,scene_time:532,bg_image_pass:"bg_9$3.gif"},
+    {scene_num:9_04,scene_time:533,bg_image_pass:"bg_9$4.gif"},
+    {scene_num:9_05,scene_time:534,bg_image_pass:"bg_9$5.jpg"},
+    {scene_num:9_06,scene_time:535,bg_image_pass:"bg_9$6.gif"},
+    {scene_num:9_07,scene_time:536,bg_image_pass:"bg_9$7.jpg"},
+    {scene_num:10_01,scene_time:610,bg_image_pass:"bg_3_10_導入_ディザリング.gif"},
+    {scene_num:10_02,scene_time:611,bg_image_pass:"bg_3_10_静止画_導入.jpg"},
+    {scene_num:10_03,scene_time:612,bg_image_pass:"bg_3_10_終了_ディザリング.gif"},
+    {scene_num:10_04,scene_time:613,bg_image_pass:"bg_3_10_静止画_終了.jpg"},
+    {scene_num:11_01,scene_time:720,bg_image_pass:"bg_11_1.gif"},
+    {scene_num:11_02,scene_time:721,bg_image_pass:"bg_11_1_静止画.jpg"},
+    {scene_num:11_03,scene_time:750,bg_image_pass:"bg_11_2.gif"},
+    {scene_num:11_04,scene_time:751,bg_image_pass:"bg_11_2_静止画.jpg"},
+    {scene_num:12,scene_time:810,bg_image_pass:"bg_5_7_12_16_18_静止画_2.jpg"},
+    {scene_num:13,scene_time:930,bg_image_pass:"bg_1_13.GIF"},
+    {scene_num:14,scene_time:1020,bg_image_pass:"bg_14_静止画.jpg"},
+    {scene_num:15,scene_time:1060,bg_image_pass:"bg_15_20_静止画.jpg"},
+    {scene_num:16,scene_time:1140,bg_image_pass:"bg_5_7_12_16_18_静止画_1.jpg"},
+    {scene_num:17,scene_time:1210,bg_image_pass:"bg_4_6_8_17_19.jpg"},
+    {scene_num:18,scene_time:1225,bg_image_pass:"bg_5_7_12_16_18_静止画_2.jpg"},
+    {scene_num:19,scene_time:1315,bg_image_pass:"bg_4_6_8_17_19.jpg"},
+    {scene_num:20,scene_time:1330,bg_image_pass:"bg_15_20_静止画.jpg"},
+    {scene_num:21,scene_time:9999,bg_image_pass:"仮背景.jpg"}
+];
+
+//追加箇所 HN
+/* アニメーションテキストを生成 */
+for(var i=0;i<phrases.length;i++){
+    animTextList.push(new AnimationTextBase(phrases[i].text,"Arial","40px","black","300px","300px",0,3000,0));
+    animTextList[i].setID("text_"+i);
+    animTextList[i].generate();
+}
+
+// 画像格納変数
+const bg_pass = "../../img/bg_img/";
+const bg_tmp = "仮背景.jpg";
+// const bg_1_13 = "bg_1_13.GIF";
+// const bg_2$1 = "bg_2_導入_アニメ.gif"
+// const bg_2$2 = "bg_2_導入_静止画.jpg"
+// const bg_2$3 = "bg_2_終わり_アニメ.gif"
+// const bg_2$4 = "bg_2_終わり_静止画.jpg"
+// const bg_3_10$1 = "bg_3_10_導入_ディザリング.gif";
+// const bg_3_10$2 = "bg_3_10_静止画_導入.jpg";
+// const bg_3_10$3 = "bg_3_10_終了_ディザリング.gif";
+// const bg_3_10$4 = "bg_3_10_静止画_終了.jpg";
+// const bg_5_7_12_16_18$sabi = "bg_5_7_12_16_18_サビ音ハメ.jpg";
+// const bg_5_7_12_16_18$1 = "bg_5_7_12_16_18_静止画_1.jpg";
+// const bg_5_7_12_16_18$2 = "bg_5_7_12_16_18_静止画_2.jpg";
+// const bg_5_7_12_16_18$3 = "bg_5_7_12_16_18_静止画_3.jpg";
+// const bg_5_7_12_16_18$4 = "bg_5_7_12_16_18_静止画_4.jpg";
+// const bg_4_6_8_17_19 = "bg_4_6_8_17_19.jpg";
+// const bg_9$1 = "bg_9$1.gif";
+// const bg_9$2 = "bg_9$2.jpg";
+// const bg_9$3 = "bg_9$3.gif";
+// const bg_9$4 = "bg_9$4.gif";
+// const bg_9$5 = "bg_9$5.jpg";
+// const bg_9$6 = "bg_9$6.gif";
+// const bg_9$7 = "bg_9$7.jpg";
+// const bg_11$1 = "bg_11_1.gif";
+// const bg_11$2 = "bg_11_1_静止画.gif";
+// const bg_11$3 = "bg_11_2.gif";
+// const bg_11$4 = "bg_11_2_静止画.gif";
+// const bg_14 = "bg_14_静止画.jpg";
+// const bg_15_20 = "bg_15_20_静止画.jpg";
+
 // APIの読み込みとPlayerクラスのインスタンスの作成
 document.addEventListener("DOMContentLoaded", () => {
     /* TextAlive Playerのインスタンスを作成 */
@@ -18,131 +221,6 @@ document.addEventListener("DOMContentLoaded", () => {
     /* 歌詞表示エリア */
     // const lyricsElement = document.getElementById("lyrics");
 
-    // 歌詞の配列データ
-    const phrases = [
-        // 歌詞と発話時間の組み合わせをここに追加
-         // １番Aメロ＃１
-
-         {text:"轟く1・2・3",time:8},
-
-         {text:"ほらよーいどんで飛び込んで",time:39},
- 
-         {text:"はらりと彩った",time:65},
- 
-         {text:"未来　未来　未来",time:88},
- 
-         {text:"Oh Oh",time:110},
- 
-         // １番Aメロ＃２
- 
-         {text:"いちにっさんしで色めいて",time:115},
- 
-         {text:"この音が巡る",time:140},
- 
-         {text:"巡るとき",time:155},
- 
-         {text:"心の全部がときめいて",time:170},
- 
-         {text:"期待したい　したい",time:190},
- 
-         // １番Bメロ
- 
-         {text:"想定外の不安要素も",time:213},
- 
-         {text:"展開は低解像度",time:240},
- 
-         {text:"伝えたい言の葉が響くまで",time:270},
- 
-         // サビ
- 
-         {text:"初の音が奏でる世界",time:315},
- 
-         {text:"彩るこの視界",time:360},
- 
-         {text:"セカイ　セカイ　セカイ",time:393},
- 
-         {text:"間違いなんかない",time:415},
- 
-         {text:"全ては君次第",time:439},
- 
-         {text:"今日は過去の未来",time:466},
- 
-         {text:"ミライ　ミライ　ミライ",time:499},
- 
-         // ーーーーーー間奏ーーーーーーー
- 
-         // ２番Aメロ
- 
-         {text:"音と音の鼓動が響き合って",time:620},
- 
-         {text:"連なって繋いで生まれ落ちて",time:650},
- 
-         {text:"記すノートも",time:675},
- 
-         {text:"痛むハートの",time:686},
- 
-         {text:"照らすライトになっていく",time:700},
- 
-         // ２番Bメロ
- 
-         {text:"ABCからXYZ",time:730},
- 
-         {text:"レイアウトは捨て去って",time:755},
- 
-         {text:"鳴らしたい音",time:778},
- 
-         {text:"伝えたいこと",time:790},
- 
-         {text:"創造も超えて宇宙の向こう側",time:802},
- 
-         // ーーーーーーー間奏ーーーーーーー
- 
-         // サビ前オチ
- 
-         {text:"初の音が奏でる世界",time:930},
- 
-         {text:"彩るこの視界",time:978},
- 
-         {text:"セカイ　セカイ　セカイ",time:1003},
- 
-         {text:"君次第",time:1030},
- 
-         {text:"全ては君の作り上げた",time:1050},
- 
-         {text:"長い長い世界の続きを見よう",time:1085},
- 
-         // ラスサビ
- 
-         {text:"High!!",time:1135},
- 
-         {text:"夢見心地のDIVE!!",time:1158},
- 
-         {text:"思うがままのLIFE!!",time:1178},
- 
-         {text:"セカイ　セカイ　セカイ",time:1210},
- 
-         {text:"間違いなんかない",time:1230},
- 
-         {text:"全ては君次第",time:1255},
- 
-         {text:"導くこの未来",time:1280},
- 
-         {text:"ミライ　ミライ　ミライ",time:1315},
- 
-         {text:"初の音が重なる世界",time:1334},
- 
-         {text:"終わりのない時代",time:1390},
-    ];
-
-    var animTextList = [];
-
-    //追加箇所 HN
-    /* アニメーションテキストを生成 */
-    for(var i=0;i<phrases.length;i++){
-        animTextList.push(new AnimationTextBase(phrases[i].text,"Arial","40px","black","300px","300px",0,3000,0));
-        animTextList[i].setID("text_"+i);
-        animTextList[i].generate();
-    }
     // 未使用↓
     // /* GSAPアニメーションのセットアップ */
     // const lyricsAnimation = gsap.fromTo("#lyrics", 
@@ -150,87 +228,12 @@ document.addEventListener("DOMContentLoaded", () => {
     //     { opacity: 1, y: 0, duration: 1, ease: "power2.out", paused: true }
     // );
 
-    var scene_info_index = 1;
-    const scene_info = [
-        {scene_num:0,scene_time:-1,bg_image_pass:"none"},
-        {scene_num:1,scene_time:0,bg_image_pass:"bg_1_13.GIF"},
-        {scene_num:2_01,scene_time:115,bg_image_pass:"bg_2_導入_アニメ.gif"},
-        {scene_num:2_02,scene_time:115+16,bg_image_pass:"bg_2_導入_静止画.jpg"},
-        {scene_num:2_03,scene_time:166,bg_image_pass:"bg_2_終わり_アニメ.gif"},
-        {scene_num:2_04,scene_time:166+11,bg_image_pass:"bg_2_終わり_静止画.jpg"},
-        {scene_num:3_01,scene_time:217,bg_image_pass:"bg_3_10_導入_ディザリング.gif"},
-        {scene_num:3_02,scene_time:217+30,bg_image_pass:"bg_3_10_静止画_導入.jpg"},
-        {scene_num:3_03,scene_time:242,bg_image_pass:"bg_3_10_終了_ディザリング.gif"},
-        {scene_num:3_04,scene_time:242+20,bg_image_pass:"bg_3_10_静止画_終了.jpg"},
-        {scene_num:4,scene_time:313,bg_image_pass:"bg_4_6_8_17_19.jpg"},
-        {scene_num:5,scene_time:345,bg_image_pass:"bg_5_7_12_16_18_静止画_1.jpg"},
-        {scene_num:6,scene_time:392,bg_image_pass:"bg_4_6_8_17_19.jpg"},
-        {scene_num:7,scene_time:412,bg_image_pass:"bg_5_7_12_16_18_静止画_2.jpg"},
-        {scene_num:8,scene_time:482,bg_image_pass:"bg_4_6_8_17_19.jpg"},
-        {scene_num:9_01,scene_time:530,bg_image_pass:"bg_9$1.gif"},
-        {scene_num:9_02,scene_time:531,bg_image_pass:"bg_9$2.jpg"},
-        {scene_num:9_03,scene_time:532,bg_image_pass:"bg_9$3.gif"},
-        {scene_num:9_04,scene_time:533,bg_image_pass:"bg_9$4.gif"},
-        {scene_num:9_05,scene_time:534,bg_image_pass:"bg_9$5.jpg"},
-        {scene_num:9_06,scene_time:535,bg_image_pass:"bg_9$6.gif"},
-        {scene_num:9_07,scene_time:536,bg_image_pass:"bg_9$7.jpg"},
-        {scene_num:10_01,scene_time:610,bg_image_pass:"bg_3_10_導入_ディザリング.gif"},
-        {scene_num:10_02,scene_time:611,bg_image_pass:"bg_3_10_静止画_導入.jpg"},
-        {scene_num:10_03,scene_time:612,bg_image_pass:"bg_3_10_終了_ディザリング.gif"},
-        {scene_num:10_04,scene_time:613,bg_image_pass:"bg_3_10_静止画_終了.jpg"},
-        {scene_num:11_01,scene_time:720,bg_image_pass:"bg_11_1.gif"},
-        {scene_num:11_02,scene_time:721,bg_image_pass:"bg_11_1_静止画.jpg"},
-        {scene_num:11_03,scene_time:750,bg_image_pass:"bg_11_2.gif"},
-        {scene_num:11_04,scene_time:751,bg_image_pass:"bg_11_2_静止画.jpg"},
-        {scene_num:12,scene_time:810,bg_image_pass:"bg_5_7_12_16_18_静止画_2.jpg"},
-        {scene_num:13,scene_time:930,bg_image_pass:"bg_1_13.GIF"},
-        {scene_num:14,scene_time:1020,bg_image_pass:"bg_14_静止画.jpg"},
-        {scene_num:15,scene_time:1060,bg_image_pass:"bg_15_20_静止画.jpg"},
-        {scene_num:16,scene_time:1140,bg_image_pass:"bg_5_7_12_16_18_静止画_1.jpg"},
-        {scene_num:17,scene_time:1210,bg_image_pass:"bg_4_6_8_17_19.jpg"},
-        {scene_num:18,scene_time:1225,bg_image_pass:"bg_5_7_12_16_18_静止画_2.jpg"},
-        {scene_num:19,scene_time:1315,bg_image_pass:"bg_4_6_8_17_19.jpg"},
-        {scene_num:20,scene_time:1330,bg_image_pass:"bg_15_20_静止画.jpg"},
-        {scene_num:21,scene_time:9999,bg_image_pass:"仮背景.jpg"}
-    ];
-
     /* 再生ボタン */
     var scene = 0;
     var animTextIndex = 0;
     const bg_image = $('#bg_image');
     const bg_image2 = $('#bg_image2');
     $('#bg_image2').hide();
-    // 画像格納変数
-    const bg_pass = "../../img/bg_img/";
-    const bg_tmp = "仮背景.jpg";
-    // const bg_1_13 = "bg_1_13.GIF";
-    // const bg_2$1 = "bg_2_導入_アニメ.gif"
-    // const bg_2$2 = "bg_2_導入_静止画.jpg"
-    // const bg_2$3 = "bg_2_終わり_アニメ.gif"
-    // const bg_2$4 = "bg_2_終わり_静止画.jpg"
-    // const bg_3_10$1 = "bg_3_10_導入_ディザリング.gif";
-    // const bg_3_10$2 = "bg_3_10_静止画_導入.jpg";
-    // const bg_3_10$3 = "bg_3_10_終了_ディザリング.gif";
-    // const bg_3_10$4 = "bg_3_10_静止画_終了.jpg";
-    // const bg_5_7_12_16_18$sabi = "bg_5_7_12_16_18_サビ音ハメ.jpg";
-    // const bg_5_7_12_16_18$1 = "bg_5_7_12_16_18_静止画_1.jpg";
-    // const bg_5_7_12_16_18$2 = "bg_5_7_12_16_18_静止画_2.jpg";
-    // const bg_5_7_12_16_18$3 = "bg_5_7_12_16_18_静止画_3.jpg";
-    // const bg_5_7_12_16_18$4 = "bg_5_7_12_16_18_静止画_4.jpg";
-    // const bg_4_6_8_17_19 = "bg_4_6_8_17_19.jpg";
-    // const bg_9$1 = "bg_9$1.gif";
-    // const bg_9$2 = "bg_9$2.jpg";
-    // const bg_9$3 = "bg_9$3.gif";
-    // const bg_9$4 = "bg_9$4.gif";
-    // const bg_9$5 = "bg_9$5.jpg";
-    // const bg_9$6 = "bg_9$6.gif";
-    // const bg_9$7 = "bg_9$7.jpg";
-    // const bg_11$1 = "bg_11_1.gif";
-    // const bg_11$2 = "bg_11_1_静止画.gif";
-    // const bg_11$3 = "bg_11_2.gif";
-    // const bg_11$4 = "bg_11_2_静止画.gif";
-    // const bg_14 = "bg_14_静止画.jpg";
-    // const bg_15_20 = "bg_15_20_静止画.jpg";
     const playButton = document.getElementById("play-button");
     playButton.addEventListener("click", () => {
         player.requestPlay(); // 再生を要求する
@@ -254,9 +257,9 @@ document.addEventListener("DOMContentLoaded", () => {
         },100)//ここまで HN
     });
 
-    function setSceneBackGround(index,bg_num){
-        if(index == 1) bg_image.attr('src',bg_pass + bg_num + "?" + (new Date).getTime());
-        else bg_image2.attr('src',bg_pass + bg_num + "?" + (new Date).getTime());
+    function setSceneBackGround(index,pass){
+        if(index == 1) bg_image.attr('src',bg_pass + pass);
+        else bg_image2.attr('src',bg_pass + pass);
     }
 
     /* ロード完了後にロード画面を非表示にする */
@@ -329,6 +332,19 @@ document.addEventListener("DOMContentLoaded", () => {
         // マウス、キーボードの操作によるエフェクト発生↓（後述）
     });
 });
+
+//画像を先に読み込む処理
+window.onload = function(){
+    loadImages();
+}
+
+// 画像プリロード用関数
+function loadImages(){
+    for (i = 1; i < scene_info.length; i++){
+        var img = document.createElement('img');
+        img.src = bg_pass+scene_info[i].bg_image_pass;
+    }
+}
 
 //以下キャッシュ無効化の処理
 window.onbeforeunload = function() {};
