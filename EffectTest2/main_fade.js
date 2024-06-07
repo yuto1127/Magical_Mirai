d862a1b6adaf19fa89ccc0f48c62880e9ba1b090
f23aa005-oike <168510791+f23aa005-oike@users.noreply.github.com>
2024-06-07 23:15:50 +0900

    fade

--- EffectTest2/main_fade.js
+++ EffectTest2/main_fade.js
@@ -1,21 +1,7 @@
 console.log("test_fade");
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js" crossorigin="anonymous"></script>
-$(window).on('load', function(){
-    $('body').removeClass('fadeout');
-  });
+<script>
 
-  $(function() {
-    // ハッシュリンク(#)と別ウィンドウでページを開く場合はスルー
-    $('a:not([href^="#"]):not([target])').on('click', function(e){
-      e.preventDefault(); // ナビゲートをキャンセル
-      url = $(this).attr('href'); // 遷移先のURLを取得
-      if (url !== '') {
-        $('body').addClass('fadeout');  // bodyに class="fadeout"を挿入
-        setTimeout(function(){
-          window.location = url;  // 0.8秒後に取得したURLに遷移
-        }, 800);
-      }
-      return false;
-    });
-  });
+$('div').fadeIn(1500)
+
+</script>
