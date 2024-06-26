2262a293061f054ac094805891b508cbdfd6994a
f23aa005-oike <168510791+f23aa005-oike@users.noreply.github.com>
2024-06-26 14:07:52 +0900

    test

--- EffectTest2/main.progressbar.js
+++ EffectTest2/main.progressbar.js
@@ -1,7 +1,11 @@
-var circle = new ProgressBar.Circle('#progress', {
-  color: '#FCB03C',
-  duration: 3000,
-  easing: 'easeInOut'
+var bar = new ProgressBar.Line(container, {
+  strokeWidth: 4,
+  easing: 'easeInOut',
+  duration: 1400,
+  color: '#FFEA82',
+  trailColor: '#eee',
+  trailWidth: 1,
+  svgStyle: {width: '100%', height: '100%'}
 });
 
-circle.animate(1); 
+bar.animate(1.0); 
