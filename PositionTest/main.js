0a95020f4862ec3ddbd815eb03084eba3967cce6
HN-YM <166769807+HN-YM@users.noreply.github.com>
2024-07-05 00:16:08 +0900

    test

--- PositionTest/main.js
+++ PositionTest/main.js
@@ -434,7 +434,7 @@ let yPos = 0;
 img.onload = () => {
     function animate() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.drawImage(img, 0, 0,img.width,img.height,0,yPos,canvas.width,canvas.height);
+        ctx.drawImage(img, 0, 0,img.width,img.height,0,yPos,canvas.width,img.height * Math.round(canvas.width / img.width * 100) / 100);
 
         yPos -= 1; // 画像の移動速度
         if (yPos  > -(img.height)) {
